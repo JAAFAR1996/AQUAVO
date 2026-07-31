35377e070fa67242fbf3bcf806cda6918604e43b
JAAFAR1996 <138117801+JAAFAR1996@users.noreply.github.com>
2026-07-31 16:40:14 +0300

    fix(ui): load AQUAVO confirmed token overrides

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/aquavo-ui-fixes.css";
 import { initializeClientEnvSideEffects } from "./lib/config/env";
 
 initializeClientEnvSideEffects();
