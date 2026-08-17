48907846cf37e4f6cb5b3e08991b909f23c6cd55
JAAFAR1996 <138117801+JAAFAR1996@users.noreply.github.com>
2026-08-17 16:57:56 +0300

    Track rollback of admin order purge migration

--- migrations/0076_admin_non_delivered_order_purge_rollback.sql
+++ migrations/0076_admin_non_delivered_order_purge_rollback.sql
@@ -1,1 +1,10 @@
+BEGIN;
+
 DROP FUNCTION IF EXISTS public.purge_non_delivered_order(text,text,text);
+
+UPDATE public.schema_migrations
+SET rolled_back_at=clock_timestamp(),
+    notes=COALESCE(notes,'')||' [admin non-delivered order purge support rolled back]'
+WHERE version='0076_admin_non_delivered_order_purge';
+
+COMMIT;
